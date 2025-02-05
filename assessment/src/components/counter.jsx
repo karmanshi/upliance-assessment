4c15f21451e871549ff8d2fdd853e900c2185814
Karmanshi Shrivastava <80811897+karmanshi@users.noreply.github.com>
2025-02-05 15:23:16 +0000

    Added persistance in counter and text editor using localstorage

--- assessment/src/components/counter.jsx
+++ assessment/src/components/counter.jsx
@@ -1,16 +1,40 @@
-import {useState} from "react";
-import Button from "./button";
+import {useEffect, useState} from "react";
+import Button from "./common/button";
 
 const CounterComponent = () => {
   const [count, setCount] = useState(0);
+
+  useEffect(()=>{
+    const countNumber=localStorage.getItem("count_number")||0
+    setCount(parseInt(countNumber))
+  }, [])
+
+  const updateData=(condition)=>{
+    let updateValue=count
+    switch (condition) {
+      case "increase":
+        updateValue=count + 1
+        break;
+      case "decrease":
+        updateValue=count - 1
+        break;
+        
+      default:
+        updateValue=0
+        break;
+    }
+    setCount(updateValue)
+    localStorage.setItem("count_number", updateValue)
+  }
+
   return (
     <div className="flex flex-col gap-10 justify-center w-full p-10 border border-2 rounded-md">
       <div className="w-full flex justify-center font-bold">{count}</div>
       <div className="w-full flex justify-center font-bold">COUNTER</div>
       <div className="w-full flex justify-center gap-5">
-        <Button onClick={() => setCount(count + 1)} extra_css="bg-blue-400 hover:bg-blue-600" text="+" />
-        <Button onClick={() => setCount(0)} extra_css="bg-gray-400 hover:bg-gray-600" text="Reset" />
-        <Button onClick={() => setCount(count - 1)} extra_css="bg-blue-400 hover:bg-blue-600" text="-" />
+        <Button onClick={() => updateData('increase')} extra_css="bg-blue-400 hover:bg-blue-600" text="+" />
+        <Button onClick={() => updateData('reset')} extra_css="bg-gray-400 hover:bg-gray-600" text="Reset" />
+        <Button onClick={() => updateData('decrease')} extra_css="bg-blue-400 hover:bg-blue-600" text="-" />
       </div>
     </div>
   );
--- assessment/src/components/texteditor.jsx
+++ assessment/src/components/texteditor.jsx
@@ -1,10 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const TexteditorComponent = (props) => {
-  const [textData, setTextData]=useState("Start typing here.....")
-  return (
-    <textarea className="p-4 w-full border border-2 rounded-md" cols={8} rows={8} onChange={(e)=>setTextData(e.target.value)}>{textData}</textarea>
-  )
-}
+  const [textData, setTextData] = useState("");
 
-export default TexteditorComponent
+  useEffect(() => {
+    const storedTextData=localStorage.getItem("text_editor_content")||"Start typing here....."
+    setTextData(storedTextData)
+  }, []);
+
+  const handleTextInputChange = (e) => {
+    setTextData(e.target.value);
+    localStorage.setItem("text_editor_content", e.target.value)
+  };
+
+  return (
+    <textarea
+      className="p-4 w-full border border-2 rounded-md"
+      cols={8}
+      rows={8}
+      onChange={handleTextInputChange}
+      value={textData}
+    >
+    </textarea>
+  );
+};
+
+export default TexteditorComponent;
